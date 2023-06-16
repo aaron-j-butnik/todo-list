499408e9c7628c41d87deb666a7d3a7dde7a4f7e
Aaron Butnik <aaronjbutnik@gmail.com>
2023-06-15 18:54:56 -0700

    Installed npm mongoClient.  Added connection code to server.js, added DB_STRING connection to .env file. Tested and verified that connection works.

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
 const app = express();
 const PORT = 3000;
 require("dotenv").config();
 
+let db;
+let dbConnectionStr = process.env.DB_STRING;
+let dbName = "todo-list";
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
+  (client) => {
+    console.log(`Connected to ${dbName} Database`);
+    db = client.db(dbName);
+  }
+);
+
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
